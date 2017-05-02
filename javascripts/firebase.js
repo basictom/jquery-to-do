192f7e8cf8d3c76231a3335363e7bb5bae22e6cb
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-05-02 18:40:02 -0500

    adding what I got

--- javascripts/firebase.js
+++ javascripts/firebase.js
@@ -5,24 +5,22 @@ var FbAPI = (() => {
 
 
   return {
-    todoGetter : () => {
-      return todos;
-    },
-    setTodos : (newArray) => {
-      todos = newArray;
-    },
-    setSingleTodo: (newObject) => {
-      todos.push(newObject);
-    },
-    setChecked: (itemId) => {
-      const position = itemId.split("item")[1];
-      todos[position].isCompleted = !todos[position].isCompleted;
-    },
-    duhlete: (id) => {
-      //item1
-      const position = id.split("item")[1];
-      todos.splice(position, 1);//(where in the array, and how many you want from it)
-    }
+
+
+      firebaseCreds : () => {
+     			return new Promise((resolve, reject) => {
+     				$.ajax("apiKeys.json" )
+     				.done((data) => {
+              console.log("firebase js", data);
+     					resolve(data);
+     				})
+     				.fail((error) => {
+              console.log("firebase js", error);
+     					reject(error);
+     				});
+     		});
+
+      }
   };
 
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  let apiKeys;
+  let editId = "";
 
   $("#new-item").on("click", () => {
     $('.list-container').addClass('hide');
@@ -10,42 +12,48 @@ $(document).ready(function(){
     $('.list-container').removeClass('hide');
   });
 
+  FbAPI.firebaseCreds().then((keys) => {
+    apiKeys = keys;
+    firebase.initializeApp(apiKeys);
+    FbAPI.writeDom(apiKeys);
+  }).catch((error) => {
+    console.log("error in firebase", error);
+  });
+
   $('#add-todo-button').on("click", () => {
     let newTodo = {
       isCompleted: false,
       task: $('#add-todo-text').val()
     };
-    console.log("newtodo", newTodo);
-    FbAPI.addTodo(newTodo).then(() => {
-      $('#add-todo-text').val("");
-      $('.new-container').addClass('hide');
-      $('.list-container').removeClass('hide');
-      FbAPI.writeDom();
-      countTask();
-    }).catch((error) => {
-      console.log("add to error", error);
-    });
+
+    if(editId.length > 0){
+      FbAPI.addTodo(apiKeys, newTodo, editId).then(() => {
+        $('#add-todo-text').val("");
+        editId = "";
+        $('.new-container').addClass('hide');
+        $('.list-container').removeClass('hide');
+        FbAPI.writeDom(apiKeys);
+      }).catch((error) => {
+        console.log("add to error", error);
+      });
+    }else{
+      FbAPI.addTodo(apiKeys, newTodo).then(() => {
+        $('#add-todo-text').val("");
+        $('.new-container').addClass('hide');
+        $('.list-container').removeClass('hide');
+        FbAPI.writeDom(apiKeys);
+      }).catch((error) => {
+        console.log("add to error", error);
+      });
+    }
   });
-
-
-
-  FbAPI.getTodos()
-  .then(() => {
-    FbAPI.writeDom();
-    countTask();
-  })
-  .catch((error) => {
-    console.log("get todos error", error);
-  });
-
 
   // Delete function
 
   $('.main-container').on('click', '.delete', (event) => {
-    FbAPI.deleteTodo(event.target.id)
+    FbAPI.deleteTodo(apiKeys, event.target.id)
     .then(() => {
-      FbAPI.writeDom();
-      countTask();
+      FbAPI.writeDom(apiKeys);
     }).catch((error) => {
       console.log("error in the delete to do", error);
     });
@@ -55,6 +63,7 @@ $(document).ready(function(){
 
   $('.main-container').on('click', '.edit', (event) => {
     let editText = $(event.target).closest('.col-xs-4').siblings('.col-xs-8').find('.task').html();
+    editId = event.target.id;
     FbAPI.editTodo(event.target.id)
     .then(() =>{
       $('.list-container').addClass('hide');
@@ -69,16 +78,12 @@ $(document).ready(function(){
 
   $('.main-container').on("click", 'input[type=checkbox]', (event) => {
     FbAPI.checker(event.target.id).then(() => {
-      FbAPI.writeDom();
-      countTask();
+      FbAPI.writeDom(apiKeys);
     }).catch((error) => {
       console.log("error checker", error);
     });
   });
 
-  let countTask = () => {
-    let remainingTasks = $('#incomplete-tasks li').length;
-    $('#counter').hide().fadeIn(1000).html(remainingTasks);
-  };
+
 
 });
